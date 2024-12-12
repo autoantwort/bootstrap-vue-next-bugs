9fda24486af87887470c8cdb492b93c014c658ef
autoantwort <Leander.schulten@rwth-aachen.de>
2024-12-12 18:28:20 +0100

    add code

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1>{{ message }}</h1>
+    <BButton @click="show?.({ props: { title: 'Hello', body: 'World' } })">Show</BButton>
+    <BToastOrchestrator />
+  </div>
+</template>
+
+
+<script>
+import { useToastController } from 'bootstrap-vue-next';
+const { show } = useToastController()
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: 'Hello Vue!'
+    }
+  }
+}
+</script>
